f4889fc1faab5149ff16a737252ad107acd17355
dennyleudy <le_udy@hotmail.com>
2017-03-20 13:45:36 +0100

    t

---
+++ application/views/inicio.php
@@ -0,0 +1,40 @@
+<html>
+<hed>
+		<title>fatwir</title>
+
+</head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<style>
+
+</style>
+<body >
+	<div class="">
+	  <div class="col col-sm4 col-cm-offset-4">
+	    <form method="post" action="">
+	      <h2>Ingresar</h2>
+	      <div class="form-group input-group">
+	        <label class="input-group-addon">Usuario</label>
+	        <input type="txt" name="email" class="form-control"/>
+	      </div>
+	      <div class="form-group input-group">
+	        <label class="input-group-addon">Clave</label>
+	        <input type="password" name="clave" class="form-control"/>
+	      </div>
+	      <div style color="red">
+	      </div>
+	      <div class="txt-center">
+	        <button class="btn btn-primary">Ingresar</button>
+	      </div>
+	    </form>
+	  </div>
+	</div>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,13 @@
+<?php  ?>
+
+<html>
+<head>
+  <title>Prueva</title>
+</head>
+
+<bodyy>
+  <h1>Registrate y sigue el chat.</h1>
+  <div>
+  </div>
+</body>
+</html>
